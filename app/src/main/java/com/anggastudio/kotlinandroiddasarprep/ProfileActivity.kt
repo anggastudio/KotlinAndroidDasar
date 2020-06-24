602a23a9ca048498df1f0c49dd751b877d88d30e
anggastudio <aa.angga.pratama@gmail.com>
2020-06-24 12:50:13 +0700

    initial design of profile screen

--- app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
+++ app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
@@ -3,8 +3,10 @@ package com.anggastudio.kotlinandroiddasarprep
 import android.os.Bundle
 
 class ProfileActivity : BaseActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
     }
+
 }
