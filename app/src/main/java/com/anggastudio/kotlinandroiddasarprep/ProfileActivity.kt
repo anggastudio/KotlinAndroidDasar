1711471f1df80a79f868d2ef303163491281cbb5
anggastudio <aa.angga.pratama@gmail.com>
2020-06-24 13:27:07 +0700

    added image pciker dependency

--- app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
+++ app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
@@ -1,6 +1,7 @@
 package com.anggastudio.kotlinandroiddasarprep
 
 import android.os.Bundle
+import com.esafirm.imagepicker.features.ImagePicker
 import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : BaseActivity() {
@@ -9,7 +10,13 @@ class ProfileActivity : BaseActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
 
+        ivAvatar.setOnClickListener { openImagePicker() }
         btnSignOut.setOnClickListener { signOut() }
+    }
+
+    private fun openImagePicker() {
+        ImagePicker.create(this) // Activity or Fragment
+            .start();
     }
 
     private fun signOut() {
