6249ad1eeb15bc947408ce58a793d0bf67b79879
anggastudio <aa.angga.pratama@gmail.com>
2020-06-24 13:13:09 +0700

    added sign out functionality

--- app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
+++ app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
@@ -1,12 +1,19 @@
 package com.anggastudio.kotlinandroiddasarprep
 
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_profile.*
 
 class ProfileActivity : BaseActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_profile)
+
+        btnSignOut.setOnClickListener { signOut() }
+    }
+
+    private fun signOut() {
+        gotoLoginActivity();
     }
 
 }
