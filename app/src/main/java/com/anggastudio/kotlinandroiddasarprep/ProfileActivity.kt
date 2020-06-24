e5460d51aebdfbec4b40977b431fde059703638b
anggastudio <aa.angga.pratama@gmail.com>
2020-06-24 15:00:15 +0700

    show image as circle using glide

--- app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
+++ app/src/main/java/com/anggastudio/kotlinandroiddasarprep/ProfileActivity.kt
@@ -1,6 +1,9 @@
 package com.anggastudio.kotlinandroiddasarprep
 
+import android.content.Intent
 import android.os.Bundle
+import com.bumptech.glide.Glide
+import com.bumptech.glide.load.resource.bitmap.CircleCrop
 import com.esafirm.imagepicker.features.ImagePicker
 import kotlinx.android.synthetic.main.activity_profile.*
 
@@ -21,6 +24,17 @@ class ProfileActivity : BaseActivity() {
 
     private fun signOut() {
         gotoLoginActivity();
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        if (ImagePicker.shouldHandle(requestCode, resultCode, data)) {
+            val image = ImagePicker.getFirstImageOrNull(data)
+            Glide.with(this)
+                .load(image.path)
+                .transform(CircleCrop())
+                .into(ivAvatar)
+        }
+        super.onActivityResult(requestCode, resultCode, data)
     }
 
 }
